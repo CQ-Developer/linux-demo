f1a3f2bf923f87e0ea959b33eea398b131f90d2e
chen <18551681083@163.com>
2023-02-17 16:08:01 +0800

    在变量引用时判空并给与默认值

--- shellscript/pi.sh
+++ shellscript/pi.sh
@@ -6,5 +6,8 @@ echo "This program will calculate PI."
 # 读取用户需要获取的PI的小数位
 read -p "scale number 10-1000: " len
 
+# 防止len为空
+scale=${len:-10}
+
 # 执行计算并输出计算事件
-time echo "scale=${len}; 4*a(1)" | bc -l
+time echo "scale=${scale}; 4*a(1)" | bc -l
