0907382567b21f1d35aaed3afbaf7777368f3319
chen <18551681083@163.com>
2023-02-20 13:16:51 +0800

    实现最基本的function功能

---
+++ shellscript/show_123_2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# 只允许选择 1 2 3
+
+function print_it() {
+    # -n 是输出不换行
+    echo -n "your choice is "
+}
+
+echo "This program will print your selection"
+case $1 in
+    "one")
+        print_it; echo $1 | tr [:lower:] [:upper:]
+        ;;
+    "two")
+        print_it; echo $1 | tr [:lower:] [:upper:]
+        ;;
+    "three")
+        print_it; echo $1 | tr [:lower:] [:upper:]
+        ;;
+    *)
+        echo "usage $0 {one | two | three}"
+esac
+
+exit 0
