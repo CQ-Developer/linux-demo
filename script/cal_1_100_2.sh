aac02441e79a7d2319ef7d60852180b55f5651c1
chen <18551681083@163.com>
2023-02-20 15:10:42 +0800

    添加一些格式化

--- script/cal_1_100_2.sh
+++ script/cal_1_100_2.sh
@@ -6,9 +6,9 @@ echo "input your num, i will accumulate from 1 to this num"
 read -p "input: " input_num
 
 accumulate_result=0
-for ((i=1; i<=$input_num; i++))
+for ((i = 1; i <= $input_num; i++))
 do
-    accumulate_result=$(($accumulate_result+$i))
+    accumulate_result=$(($accumulate_result + $i))
 done
 
-echo "1+2+...+$input_num=$accumulate_result"
+echo "1 + 2 + ... + $input_num = $accumulate_result"
